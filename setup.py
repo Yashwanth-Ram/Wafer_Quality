cadb433001d8832be4c3b581d353c8e64d9ae167
Yashwanth-Ram <yashwanthram786@gmail.com>
2022-11-06 16:44:06 +0530

    A few changes to the UI

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="wafer-fault-detection",
+    name="wafer-fault-detection-project",
     version="0.0.3",
     description="ML project",
-    author="Avnish yadav", 
+    author="Yashwanth Ram",
     packages=find_packages(),
     license="MIT"
 )
